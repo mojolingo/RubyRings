eeec06148350a455be83da52b22552213cfc4b9b
Rene Mendoza <rmendoza@mojolingo.com>
2012-02-17 11:56:49 -0600

    added rails skeleton with cucumber, rspec, formtastic, capistrano, compass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyRings::Application.config.secret_token = 'df109377c4e25d6098fd1b8da47bbb3cb06abd8ff90018f266e5fdb764025201290417640bcfc77ed5ca82c611e6c978176ecda9f03d28beb99c18c0247e63a3'
